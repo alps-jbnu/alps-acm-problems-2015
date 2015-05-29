ecded1982dd78b433a271c5e5f39d50465c5ef6f
Jaewon Song <sjw0687@gmail.com>
2015-05-30 05:00:00 +0900

    a2Z Number - 오버플로 방지하도록 입력 생성기 수정

--- a2Z Number/generator.cpp
+++ a2Z Number/generator.cpp
@@ -1,48 +1,51 @@
-/***
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
 
-void f(int k, int len){
+void f(int k, int len) {
     printf("%d ", k);
-    for(int i=0; i<len; ++i){
-        int p = rand()%k;
-        if(i==0) while(p==0) p=rand()%k;
-        if(p<10) p = '0'+p;
-        else if(p<36) p = 'A'+(p-10)%10;
-        else p = 'a'+(p-36)%10;
+
+    for (int i = 0; i < len; ++i) {
+        // 현재 자릿수
+        int p = rand() % k;
+
+        // 첫 번째 자리 0 방지
+        if (i == 0)
+            while (p == 0)
+                p = rand() % k;
+
+        if (p < 10)
+            p = '0' + p;
+        else if (p < 36)
+            p = 'A' + (p - 10);
+        else
+            p = 'a' + (p - 36);
+
         printf("%c", p);
     }
-    puts("");
+
+    printf("\n");
 }
 
-int main(){
+int main() {
     int i, k, len;
     srand(time(NULL));
-    for(i=0; i<50; ++i) f(rand()%60+2, rand()%30+1);
-    return 0;
-}
 
-// 우선 만들어 보고
-***/
-/****/
-// 데이터 추려내기
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-int main(){
-    char s[32];
-    int i, T, x;
-    scanf("%d", &T);
-    while(T--){
-        long long sum=0, p=1;
-        bool can=true;
-        std::cin >> x >> s;
-        for(i=strlen(s)-1;i>=0;--i,p*=x){
-            sum += p * (s[i]-(s[i]<'A'?'0':(s[i]<'a'?55:49)));
-        }
-        if(sum > 0 && sum <= 2147483648) printf("%d %lld\n", x, sum);
+    for (i = 0; i < 50; ++i) {
+        int k, len;
+        k = rand() % 60 + 2;
+
+        if (k <= 8)
+            len = rand() % 9 + 1;
+        else if (k <= 15)
+            len = rand() % 6 + 1;
+        else if (k <= 31)
+            len = rand() % 5 + 1;
+        else
+            len = rand() % 4 + 1;
+
+        f(k, len);
     }
+
     return 0;
 }
-/***/
